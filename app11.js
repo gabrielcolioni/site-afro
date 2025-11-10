edde0cb136fa492db2a9ab2f1465803e896d3189
gabrielcolioni <gabriel.colioni@escola.pr.gov.br>
2025-11-10 12:04:00 -0300

    update

--- app11.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  // Exemplo de interação simples, como mostrar um alerta ao clicar em "Informações"
-  const infoButton = document.querySelector('#informacoes');
-  infoButton.addEventListener('click', function() {
-    alert('Esta música é uma crítica social profunda! Escute com atenção.');
-  });
-});
